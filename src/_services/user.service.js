ded7d926a294850f0a53f50d5ddaba2c3eb47868
Alquinn John Gayatao <alquinngayatao@gmail.com>
2018-10-06 07:49:38 +0800

    login

---
+++ src/_services/user.service.js
@@ -0,0 +1,122 @@
+import config from 'config';
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    login,
+    logout,
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+// gets csrf token
+function getToken(){
+    const requestOptions = {
+        method: 'GET',
+        'mode':'no-cors'
+    };
+
+    return fetch(`${config.apiUrl}/init`, requestOptions).then(handleResponse);
+}
+
+function login(username, password) {
+    getToken().then(
+        users => {
+            console.log("data: ", users);
+        }
+    );
+
+    const requestOptions = {
+        method: 'POST',
+        'mode':'no-cors',
+        headers: { 'Content-Type': 'application/json', 'X-CSRF-TOKEN': '' },
+        body: JSON.stringify({ username, password })
+    };
+
+    return fetch(`${config.apiUrl}/login`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            // login successful if there's a jwt token in the response
+            if (user.token) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+
+            return user;
+        });
+}
+
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/register`, requestOptions).then(handleResponse);
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
+}
+
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function update(user) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+        console.log("Data", data);
+        return data;
+    });
+}
--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>MAMMBA tours</h1>
-  </div>
-</template>
